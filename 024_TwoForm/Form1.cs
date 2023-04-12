eb544af5e25096f005aa3d41ee5ffdc256f4b899
imCwater <114461843+imCwater@users.noreply.github.com>
2023-04-12 15:06:37 +0900

    Add files via upload

---
+++ 024_TwoForm/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _024_TwoForm
+{
+    public partial class Form1 : Form
+    {
+        Form2 f = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(f ==null)
+                f = new Form2(this);
+            f.Show();
+            this.Hide();   //form1은 숨긴다
+        }
+    }
+}
